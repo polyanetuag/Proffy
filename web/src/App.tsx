2bd6e4699ed499cf13d3eeb5487dc88c3752ffb5
Polyane <pollyportugues@hotmail.com>
2020-08-03 19:32:59 -0300

    Criando estilos globais

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import './assets/styles/global.css';
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Hello World!</h1>
     </div>
   );
 }
