bdb4526633cc83dc2d9744e3f7a1fd330ec2443d
Polyane Tuag <pollyportugues@hotmail.com>
2020-08-06 15:19:34 -0300

    Configurando conexão com o banco de dados

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,12 +2,10 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    const users = [
-        {name: 'Ana', age: 20},
-        {name: 'Saulo', age: 32},
-    ];
-    return response.json(users);
+app.use(express.json());
+
+app.get('/', (request, response) => {
+    return response.json({ message: 'Hello World'});
 });
 
 app.listen(3333);
